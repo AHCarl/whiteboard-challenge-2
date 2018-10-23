c7c899c30ef09043db10d1e3dbbebf611012b408
AHCarl <ahcarlin@gmail.com>
2018-10-23 11:04:58 -0500

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,27 @@
 # Enter your procedural solution here!
+
+def collect_multiples(limit)
+  col = []
+  range = (1..(limit-1))
+
+  range.each do |n|
+    if n%3 == 0 || n%5 == 0 
+      col << n
+    end
+  end
+  col
+end
+
+def sum_multiples(limit)
+  running_total = 0
+  grand = (1..(limit-1))
+
+  grand.each do |n|
+    if n%3 == 0 && n%5 == 0
+      running_total += n
+    elsif n%3 == 0 || n%5 == 0
+      running_total += n
+    end
+  end
+  running_total
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,33 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_accessor :limit, :collection, :range
+
+  def initialize(limit)
+    self.limit = limit - 1
+    @collection = []
+    self.range = (1..self.limit)
+  end
+
+  def collect_multiples
+    self.range.each do |n|
+      if n%3 == 0|| n%5 == 0 
+        self.collection << n
+      end
+    end
+    self.collection
+  end
+
+  def sum_multiples
+    running_total = 0
+    self.range.each do |n|
+      if n%3 == 0 && n%5 == 0
+        running_total += n
+      elsif n%3 == 0 || n%5 == 0
+        running_total += n
+      end 
+    end 
+    running_total
+  end
+
+
+end
